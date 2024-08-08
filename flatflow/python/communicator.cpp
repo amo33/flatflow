a75ee0a86d9071612245e9f1b59894cff0da4c99
Lee Hyun Joon <andylee70044@gmail.com>
2024-08-08 13:49:47 +0900

    python: apply pybinding

---
+++ flatflow/python/communicator.cpp
@@ -0,0 +1,31 @@
+// Copyright 2024 The FlatFlow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "flatflow/rpc/communicator.h"
+#include "flatflow/rpc/communicator.grpc.fb.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(rpc, m) {
+  py::class_<flatflow::rpc::CommunicatorImpl,
+             flatflow::rpc::Communicator::Service>(m, "Communicator")
+      .def(py::init<>())
+      .def("init", &flatflow::rpc::CommunicatorImpl::Init)
+      .def("broadcast", &flatflow::rpc::CommunicatorImpl::Broadcast)
+      .def("finalize", &flatflow::rpc::CommunicatorImpl::Finalize);
+}
---
+++ setup.py
@@ -0,0 +1,73 @@
+# Copyright 2024 The FlatFlow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import subprocess
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: " +
+                ", ".join(e.name for e in self.extensions))
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+        build_args += ['--', '-j32']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        
+        cache_file = os.path.join(self.build_temp, "CMakeCache.txt")
+        if os.path.exists(cache_file):
+            os.remove(cache_file)
+        
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+setup(
+    name='rpc',
+    version='0.0.1',
+    author='MaumAI',
+    author_email='andy700@maum.ai',
+    description='A pybind11 for rpc protocol',
+    long_description='',
+    ext_modules=[CMakeExtension('rpc')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
